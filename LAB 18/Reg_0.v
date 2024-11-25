90973d3661ea9c645fa5edf5b90c80f2390aceff
Almas00222 <120727149+Almas00222@users.noreply.github.com>
2024-11-25 09:11:33 +0500

    Create Reg_0.v

---
+++ LAB 18/Reg_0.v
@@ -0,0 +1,17 @@
+module Reg_0 (
+    input wire clk,
+    input wire rst,
+    input wire en_0,
+    input wire [15:0] d_in,
+    output reg [15:0] d_out
+);
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            d_out <= 16'b0;
+        end else if (en_0) begin
+            d_out <= d_in;
+        end
+    end
+
+endmodule
