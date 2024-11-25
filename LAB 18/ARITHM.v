91502c0094d9eee673f48377483d46da85d00fa5
Almas00222 <120727149+Almas00222@users.noreply.github.com>
2024-11-25 09:11:12 +0500

    Create ARITHM.v

---
+++ LAB 18/ARITHM.v
@@ -0,0 +1,31 @@
+module ARITHM(
+  input wire [15:0] in_a,
+  input wire [15:0] in_b,
+  input wire [2:0] select,
+  output reg [15:0] alu_out
+);
+
+  reg [15:0] temp_result;
+
+always @(*) begin      
+    temp_result = 16'b0;
+    
+case (select)
+    3'b000: temp_result = (in_a) + (in_b);
+    3'b001: temp_result = (in_a) - (in_b);
+    3'b010: temp_result = (in_a) & (in_b);
+    3'b011: temp_result = (in_a) | (in_b);
+    3'b100: temp_result = (in_a) ^ (in_b);
+    3'b101: temp_result = (in_a) << (in_b);
+    3'b110: temp_result = (in_a) >> (in_b);
+    3'b111: begin
+    if (in_a == in_b) temp_result = 16'd0;
+    else if (in_a > in_b) temp_result = 16'd1;
+    else if (in_a < in_b) temp_result = 16'd2;
+    end
+    default: temp_result = 16'b0;
+    endcase
+    alu_out = temp_result;
+end
+    
+  endmodule
